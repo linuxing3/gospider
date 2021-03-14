3c4c9fd98bbc02f8b0e1df6a586ea7d4b1701b69
linuxing3 <linuxing3@qq.com>
2021-03-14 18:45:19 -0400

    added linux script

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 
 调用chrome的Api后台访问网页
 
+因为vps服务器上没有安装chrome，所以我们可以使用docker的镜像来调用
+
 ## goquery
 
 查询页面元素
@@ -15,6 +17,8 @@
 ## go prisma
 
 使用prisma的go语言客户端，更优于使用gorm等框架
+
+设置环境参数
 
 ## 使用go admin构建前端界面
 
---
+++ run.sh
@@ -0,0 +1,26 @@
+echo "======================================================================"
+echo "以下命令仅需要运行一次"
+echo "======================================================================"
+echo "数据库导入(optional)"
+echo "go run github.com/prisma/prisma-client-go introspect"
+echo "数据库迁移(optional)"
+echo "go run github.com/prisma/prisma-client-go db push --preview-feature"
+
+echo "2. 生成客户端"
+echo "go run github.com/prisma/prisma-client-go generate"
+
+echo "3. 运行"
+echo "go run main.go"
+
+echo "4. 安装gospider到$GOPATH/bin/"
+echo "go install ."
+
+echo "5. 初始化数据库, 链接如下："
+echo "postgresql://spider:20090909@db:5432/spider?schema=public"
+echo "deno run -A --unstable https://raw.githubusercontent.com/linuxing3/gospider/main/create_table.ts"
+
+echo "======================================================================"
+echo "6. 启动docker-compose"
+echo "======================================================================"
+
+docker-compose up -d --remove-orphans
