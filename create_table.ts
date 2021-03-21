f035fae55453916810d2268eaae3440011f4e7e1
linuxing3 <linuxing3@qq.com>
2021-03-21 19:42:40 -0400

    create

--- create_table.ts
+++ create_table.ts
@@ -5,6 +5,7 @@ import {
   Model,
 } from "https://deno.land/x/denodb/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { prompt, Input, Number, Confirm, Checkbox } from "https://deno.land/x/cliffy/prompt/mod.ts";
 
 import {
   User,
@@ -14,6 +15,16 @@ import {
   Flight
 } from "https://raw.githubusercontent.com/linuxing3/deno-game-monitor/develop/denodb/mock/CoreModels.ts"
 import { models } from "https://raw.githubusercontent.com/linuxing3/deno-game-monitor/develop/denodb/mock/models.index.denodb.ts"
+
+interface PostOption {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  tables?: string[],
+  procede?: boolean
+}
 
 class Movie extends Model {
   static table = "movies";
@@ -46,16 +57,100 @@ class Article extends Model {
 }
 
 const env = config({ safe: true });
-export const postOptions = {
+
+const defaultTables = [Movie, Article, User, Militant, Member, Document, Flight]
+
+const postOptions: PostOption = {
   host: "127.0.0.1",
   port: 5432,
   username: "spider",
   password: "20090909",
   database: "spider",
 };
-const connection = new PostgresConnector(postOptions);
 
-const postdb = new Database(connection)
-postdb.link([Movie, Article, User, Militant, Member, Document, Flight, ...models]);
-await postdb.sync({ drop: false });
-await postdb.close();
+const createTable = async (options: any, tables: typeof Model[]) => {
+  const postdb = new Database(new PostgresConnector(options))
+  postdb.link(tables);
+  await postdb.sync({ drop: false });
+  await postdb.close();
+}
+
+const deleteTable = async (options: any, tables: typeof Model[]) => {
+  const postdb = new Database(new PostgresConnector(options))
+  postdb.link(tables);
+  await postdb.sync({ drop: false });
+  await postdb.close();
+}
+
+async function fullMenu() {
+
+  const answers: PostOption = await prompt([{
+    name: "host",
+    message: "What's your postgresql host?",
+    type: Input,
+  }, {
+    name: "port",
+    message: "What's the port?",
+    type: Number,
+  }, {
+    name: "username",
+    message: "What's the username?",
+    type: Input,
+  }, {
+    name: "password",
+    message: "What's the password?",
+    type: Input,
+  }, {
+    name: "database",
+    message: "what's the database name?",
+    type: Input,
+  }, {
+    name: "tables",
+    message: "Select some tables",
+    type: Checkbox,
+    options: ["documents", "users", "articles", "movies"],
+  }, {
+    name: "procede",
+    message: "Do you really wan't to procede?",
+    type: Confirm,
+  }]);
+
+  const mergedOptions = { ...answers, ...postOptions }
+  console.table(mergedOptions)
+
+  if (answers.procede === true) {
+    console.log("Creating tables for  you...")
+    createTable(mergedOptions, defaultTables)
+  } else {
+    console.log("Deleting tables for  you...")
+    deleteTable(mergedOptions, defaultTables)
+  }
+}
+
+async function menu() {
+
+  const answers: PostOption = await prompt([{
+    name: "tables",
+    message: "Select some tables",
+    type: Checkbox,
+    options: ["documents", "users", "articles", "movies"],
+  }, {
+    name: "procede",
+    message: "Do you really wan't to procede?",
+    type: Confirm,
+  }]);
+
+  const mergedOptions = { ...answers, ...postOptions }
+  console.table(mergedOptions)
+
+  // TODO: choose tables model from list by table name
+  if (answers.procede === true) {
+    console.log("Creating tables for  you...")
+    createTable(mergedOptions, defaultTables)
+  } else {
+    console.log("Deleting tables for  you...")
+    deleteTable(mergedOptions, defaultTables)
+  }
+}
+
+await menu()
